aa7fa9687e45f1e117633d5e9516c2eb414fde30
Ziyad <tberziad016@gmail.com>
2025-07-12 18:35:59 +0200

    igehgerhguo

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
-
 session_start();
 
-if(isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
-    header("Location: ../ht_manager/frontend/admin/main.php");
+if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
+    header("Location: frontend/admin/main.php");
     exit();
 
-
-} 
-
-  elseif (isset($_SESSION['user'])) {
-    header("Location: ../ht_manager/frontend/user/Dashboard.php");
+} elseif (isset($_SESSION['user'])) {
+    header("Location: frontend/user/Dashboard.php");
     exit();
-} 
 
-  else {
-    header("Location: ../ht_manager/frontend/core/guist.php");
+} else {
+    header("Location: frontend/core/guist.php");
     exit();
 }
