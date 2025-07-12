7a71615081155fa084f05027dae464e9a7044f1f
Ziyad <tberziad016@gmail.com>
2025-07-12 18:40:32 +0200

    gerge

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-session_start();
-
-if (isset($_SESSION['admin']) && $_SESSION['admin'] === true) {
-    header("Location: frontend/admin/main.php");
-    exit();
-
-} elseif (isset($_SESSION['user'])) {
-    header("Location: frontend/user/Dashboard.php");
-    exit();
-
-} else {
-    header("Location: frontend/core/guist.php");
-    exit();
-}
+echo "Database connection script loaded.\n";
